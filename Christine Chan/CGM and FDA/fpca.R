d005e86919d6be71a0c48a4f10557e92cac62b5b
timothyvigers <timothy.vigers@cuanschutz.edu>
2022-12-02 07:44:21 -0700

    Create fpca.R

---
+++ Christine Chan/CGM and FDA/fpca.R
@@ -0,0 +1,6 @@
+library(refund)
+# FPCA on irregularly sampled data
+load("C:/Users/timbv/Dropbox/Work/Vigers/CF/Christine Chan/CGM and FDA/R_Objects/ydf.Rdata")
+fit = fpca.sc(ydata = na.omit(ydf))
+# Save object
+save(fit,file = "C:/Users/timbv/Dropbox/Work/Vigers/CF/Christine Chan/CGM and FDA/R_Objects/fpca_sc_fit.Rdata")
