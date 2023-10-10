483e2b2cc17a96d2536078bb6b5ce861e1fa7309
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-10-10 11:19:52 -0600

    pex separate data cleaning file

---
+++ Scott Sagel/PEx in CF/create_pex_analysis_dataset.R
@@ -0,0 +1,119 @@
+library(tidyverse)
+library(rspiro)
+library(Hmisc)
+# Import REDCap data
+redcap <- read.csv("/Users/timvigers/Dropbox/Work/Vigers/CF/Scott Sagel/PEx in CF/Data_Raw/ExacerbationsInCF222_DATA_2023-10-04_1459.csv", na.strings = "")
+# Separate demographics dataset
+demo <- redcap %>%
+  filter(redcap_event_name == "demographics_arm_1") %>%
+  select(sid, redcap_data_access_group, sex, race, ethnicity, mutation1, mutation2)
+# Format columns - race and sex should be formatted for ppFEV1 calculations
+demo$sex <- factor(demo$sex, levels = c(2, 1), labels = c("Male", "Female"))
+demo$race <- factor(demo$race,
+  levels = c(1, 2, 4, 9, 3, 5:8),
+  labels = c(
+    "White", "Black or African-American", "NE Asian", "SE Asian",
+    "Other/mixed", "Other/mixed", "Other/mixed", "Other/mixed", "Other/mixed"
+  )
+)
+demo$ethnicity <- factor(demo$ethnicity,
+  levels = 1:3,
+  labels = c(
+    "Hispanic or Latino", "Not Hispanic or Latino", "Unknown or not reported"
+  )
+)
+demo$genotype <- rowSums(demo[, c("mutation1", "mutation2")] == 1)
+demo$genotype <- factor(demo$genotype,
+  levels = 0:2,
+  labels = c("No F508del", "F508del heterozygous", "F508del homozygous")
+)
+demo[, c("mutation1", "mutation2")] <- NULL
+# Remove CHCO REDCap data - there is some weird issue with the year of admission
+# field, so Elin sent a complete file separately
+chco <- redcap %>%
+  filter(
+    redcap_data_access_group == "chco",
+    redcap_event_name != "demographics_arm_1"
+  ) %>%
+  select(
+    sid, redcap_data_access_group, redcap_repeat_instance,
+    admit_cx_results___1:admit_cx_results___9
+  )
+redcap <- redcap %>%
+  filter(redcap_data_access_group != "chco") %>%
+  # Also remove demographics
+  filter(redcap_event_name != "demographics_arm_1") %>%
+  # Limit data to necessary columns
+  select(
+    sid, redcap_data_access_group, redcap_repeat_instance,
+    admit_year:age_at_admit, height, fev1_admit, fev1_discharge,
+    admit_cx_results___1:admit_cx_results___9
+  )
+# Import supplemental data from Elin
+supplement <- readxl::read_excel("/Users/timvigers/Dropbox/Work/Vigers/CF/Scott Sagel/PEx in CF/Data_Raw/ExacerbationsInCF222-Admissions and Inpatient Meds All Fields 2023 09 29.xlsx")
+supplement <- supplement %>% select(
+  sid, redcap_repeat_instance,
+  admit_year:age_at_admit, height, fev1_admit, fev1_discharge
+)
+# Merge everything together and sort columns
+chco <- left_join(chco, supplement, by = join_by(sid, redcap_repeat_instance))
+df <- bind_rows(chco, redcap)
+df <- left_join(df, demo)
+df <- df %>% select(all_of(colnames(demo)), everything())
+# Calculate FEV1 % predicted
+df$ppfev1_admit <- pctpred_GLI(
+  age = as.numeric(df$age_at_admit),
+  height = as.numeric(df$height / 100),
+  gender = as.numeric(df$sex),
+  ethnicity = as.numeric(df$race),
+  FEV1 = as.numeric(df$fev1_admit)
+)
+df$fev1_admit <- NULL
+df$ppfev1_discharge <- pctpred_GLI(
+  age = as.numeric(df$age_at_admit),
+  height = as.numeric(df$height / 100),
+  gender = as.numeric(df$sex),
+  ethnicity = as.numeric(df$race),
+  FEV1 = as.numeric(df$fev1_discharge)
+)
+df$fev1_discharge <- NULL
+# Microbiology
+bugs <- paste0("admit_cx_results___", 1:9)
+df[, bugs] <- lapply(df[, bugs], function(c) {
+  factor(c, levels = 0:1, labels = c("Absent", "Present"))
+})
+# Site
+df$redcap_data_access_group <- factor(df$redcap_data_access_group,
+  levels = c("chco", "childrens_national", "orange_county", "rady", "seattle"),
+  labels = c("CHCO", "Children's National", "Orange County", "Rady", "Seattle")
+)
+# Labels
+labels <- list(
+  "sid" = "ID",
+  "redcap_data_access_group" = "Site",
+  "sex" = "Sex",
+  "race" = "Race",
+  "ethnicity" = "Ethnicity",
+  "genotype" = "Genotype",
+  "redcap_repeat_instance" = "Visit Number",
+  "admit_cx_results___1" = "A. xylosoxidans",
+  "admit_cx_results___2" = "B. cepacia complex",
+  "admit_cx_results___3" = "H. influenzae",
+  "admit_cx_results___4" = "Methicillin-resistant S. aureus",
+  "admit_cx_results___5" = "Methicillin-susceptible S. aureus",
+  "admit_cx_results___6" = "P. aeruginosa",
+  "admit_cx_results___7" = "P. aeruginosa mucoid",
+  "admit_cx_results___8" = "S. maltophilia",
+  "admit_cx_results___9" = "Other bacteria",
+  "admit_year" = "Year",
+  "days_admit" = "# Days in Admit",
+  "age_at_admit" = "Age",
+  "height" = "Height",
+  "ppfev1_admit" = "FEV1 % Predicted at Admit",
+  "ppfev1_discharge" = "FEV1 % Predicted at Discharge"
+)
+label(df) <- labels[colnames(df)]
+# Save
+save(df, labels,
+  file = "/Users/timvigers/Dropbox/Work/Vigers/CF/Scott Sagel/PEx in CF/Data_Cleaned/analysis_dataset.RData"
+)
