689c0f8a56502b707187bad79677efb3b1c55890
Tim Vigers <timothy.vigers@cuanschutz.edu>
2024-06-17 10:04:28 -0600

    added biocmanager

--- r_packages.R
+++ r_packages.R
@@ -1,2 +1,9 @@
 # List of regular R packages
-cran=c("tidyverse","gtsummary","styler")
+cran <- c(
+  "tidyverse", "gtsummary", "styler", "BiocManager", "rjags", "mcp",
+  "segmented", "kableExtra"
+)
+install.packages(cran)
+# Bioconductor
+bioc <- c("limma", "mixOmics")
+BiocManager::install(bioc)
