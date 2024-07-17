5557c077ed5bc777564210d1d66049e2ca8b668c
Tim Vigers <timothy.vigers@cuanschutz.edu>
2024-07-17 12:39:00 -0600

    updates for Colleen Wood finished

--- Colleen Wood/Pre-post Trikafta/clean_and_merge.R
+++ Colleen Wood/Pre-post Trikafta/clean_and_merge.R
@@ -2,16 +2,16 @@ library(tidyverse)
 library(lubridate)
 library(childsds)
 library(rspiro)
-source("/Users/timvigers/GitHub/CF/genotype_class_severity.R")
+source("/home/timvigers/GitHub/CF/genotype_class_severity.R")
 t1_variables <- c(
   "CFF ID", "Site", "Sex", "Race", "Genotypes1", "Genotypes2", "Pancreatic Status",
   "G-tube in past 12 months", "Time", "Age",
   "BMI Percentile", "Height", "Weight", "FEV1", "FVC", "HbA1c"
 )
 # CHCO
-chco_a1c <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/chco_glycemic.csv", show_col_types = F)
+chco_a1c <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/chco_glycemic.csv", show_col_types = F)
 chco_a1c$Date <- parse_date_time(chco_a1c$Date, orders = "mdy")
-chco_bmi <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/chco_bmi_pft.csv", show_col_types = F)
+chco_bmi <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/chco_bmi_pft.csv", show_col_types = F)
 chco_bmi$Date <- parse_date_time(chco_bmi$Date, orders = "mdy")
 chco <- full_join(chco_a1c, chco_bmi)
 # Fill CFF ID by MRN
@@ -22,13 +22,13 @@ chco <- chco %>%
   filter(!is.na(`CFF ID`)) %>%
   select(-MRN)
 # Add demographics, filter
-chco_reg <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/chco_registry.csv", show_col_types = F)
+chco_reg <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/chco_registry.csv", show_col_types = F)
 chco_reg$Start <- parse_date_time(chco_reg$Start, orders = "mdy")
 chco_reg$DOB <- parse_date_time(chco_reg$DOB, orders = "mdy")
 keep <- chco_reg %>%
   filter(!is.na(`CFRD Diagnosis Date`)) %>%
   pull(`CFF ID`)
-exclude <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/chco_insulin.csv", show_col_types = F)
+exclude <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/chco_insulin.csv", show_col_types = F)
 exclude <- exclude %>%
   filter(!is.na(`Remove (see notes)`)) %>%
   pull(MRN)
@@ -47,9 +47,9 @@ chco$`BMI Percentile` <- round(sds(chco$BMI,
   ref = cdc.ref
 ) * 100, 2)
 # Montana
-mon_bmi <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/montana_bmi_pft.csv", show_col_types = F)
+mon_bmi <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/montana_bmi_pft.csv", show_col_types = F)
 mon_bmi$Date <- parse_date_time(mon_bmi$Date, orders = "mdy")
-mon_a1c <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/montana_glycemic_registry.csv", show_col_types = F)
+mon_a1c <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/montana_glycemic_registry.csv", show_col_types = F)
 mon_a1c$Date <- parse_date_time(mon_a1c$Date, orders = "mdy")
 mon_a1c$DOB <- parse_date_time(mon_a1c$DOB, orders = "mdy")
 mon_a1c$HbA1c <- as.numeric(mon_a1c$HbA1c)
@@ -66,19 +66,19 @@ mon <- full_join(mon_bmi, mon_a1c)
 mon <- mon %>% filter(`CFF ID` %in% mon_starts$`CFF ID`)
 mon <- left_join(mon, mon_starts %>% select(`CFF ID`, Start))
 # Add demographics
-mon_demo <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/montana_demo.csv", show_col_types = F)
+mon_demo <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/montana_demo.csv", show_col_types = F)
 mon_demo <- mon_demo %>% select(`CFF ID`, Race, Sex)
 mon <- left_join(mon, mon_demo)
 # Add genotypes
-mon_geno <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/montana_genotypes.csv", show_col_types = F)
+mon_geno <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/montana_genotypes.csv", show_col_types = F)
 mon <- left_join(mon, mon_geno)
 # Add pancreatic and g tube status
-mon_pan <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/montana_pancreatic_insufficient.csv", show_col_types = F)
+mon_pan <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/montana_pancreatic_insufficient.csv", show_col_types = F)
 mon_pan <- mon_pan %>%
   select(-Date) %>%
   distinct()
 mon_pan$`Pancreatic Status` <- "Insufficient"
-mon_gtube <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/montana_gtubes.csv", show_col_types = F)
+mon_gtube <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/montana_gtubes.csv", show_col_types = F)
 mon_gtube <- mon_gtube %>%
   select(-Feeding, -Date) %>%
   distinct()
@@ -102,16 +102,16 @@ mon$`BMI Percentile` <- round(sds(mon$BMI,
   ref = cdc.ref
 ) * 100, 2)
 # UM
-um_a1c <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/um_a1cs.csv", show_col_types = F)
+um_a1c <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/um_a1cs.csv", show_col_types = F)
 um_a1c$Age <- round(um_a1c$Age / 365.25, 1)
-um_bmi <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/um_bmi.csv", show_col_types = F)
+um_bmi <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/um_bmi.csv", show_col_types = F)
 um_bmi$Age <- round(um_bmi$Age / 365.25, 1)
-um_ogtt <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/um_ogtt.csv", show_col_types = F)
+um_ogtt <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/um_ogtt.csv", show_col_types = F)
 um_ogtt$Age <- round(um_ogtt$Age / 365.25, 1)
-um_pft <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/um_pfts.csv", show_col_types = F)
+um_pft <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/um_pfts.csv", show_col_types = F)
 um_pft$Age <- round(um_pft$Age / 365.25, 1)
-um_race <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/um_race.csv", show_col_types = F)
-um_demo <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/um_demo.csv", show_col_types = F)
+um_race <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/um_race.csv", show_col_types = F)
+um_demo <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/um_demo.csv", show_col_types = F)
 keep <- um_demo %>%
   filter(`CFRD Status` == "CFRD with or without fasting hyperglycemia") %>%
   pull(`CFF ID`)
@@ -134,16 +134,16 @@ um$`BMI Percentile` <- round(sds(um$BMI,
   ref = cdc.ref
 ) * 100, 2)
 # UW
-uw_bmi <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/uw_bmi.csv", show_col_types = F)
+uw_bmi <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/uw_bmi.csv", show_col_types = F)
 uw_bmi$Date <- parse_date_time(uw_bmi$Date, orders = "mdy")
-uw_glyc <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/uw_glycemic.csv", show_col_types = F)
+uw_glyc <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/uw_glycemic.csv", show_col_types = F)
 uw_glyc$Date <- parse_date_time(uw_glyc$Date, orders = "mdy")
-uw_pft <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/uw_pft.csv", show_col_types = F)
+uw_pft <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/uw_pft.csv", show_col_types = F)
 uw_pft$Date <- parse_date_time(uw_pft$Date, orders = "mdy")
-uw_demo <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/uw_demo.csv", show_col_types = F)
+uw_demo <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/uw_demo.csv", show_col_types = F)
 uw_demo <- uw_demo %>% select(`CFF ID`, Race, Sex)
 # Get CFRD status and start date
-uw_start <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/uw_mods.csv", show_col_types = F)
+uw_start <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/uw_mods.csv", show_col_types = F)
 uw_start$`Date of Encounter` <- parse_date_time(uw_start$`Date of Encounter`, orders = "mdy")
 uw_start <- uw_start %>%
   arrange(`CFF ID`, `Date of Encounter`) %>%
@@ -151,7 +151,7 @@ uw_start <- uw_start %>%
   filter(row_number() == 1) %>%
   select(`CFF ID`, `Date of Encounter`) %>%
   rename(Start = `Date of Encounter`)
-uw_cfrd <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/uw_cfrd.csv", show_col_types = F)
+uw_cfrd <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/uw_cfrd.csv", show_col_types = F)
 uw_cfrd <- uw_cfrd %>%
   filter(`CFRD Status` == "CFRD with or without fasting hyperglycemia") %>%
   select(`CFF ID`) %>%
@@ -159,15 +159,15 @@ uw_cfrd <- uw_cfrd %>%
   pull(`CFF ID`)
 keep <- unique(uw_cfrd, uw_start$`CFF ID`)
 # Add genotypes
-uw_geno <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/uw_genotypes.csv", show_col_types = F)
+uw_geno <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/uw_genotypes.csv", show_col_types = F)
 # Pancreatic status
-uw_pan <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/uw_pancreatic.csv", show_col_types = F)
+uw_pan <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/uw_pancreatic.csv", show_col_types = F)
 uw_pan <- uw_pan %>%
   select(-Date) %>%
   distinct() %>%
   mutate(`Pancreatic Status` = "Insufficient")
 # G tubes
-uw_gtube <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/uw_gtubes.csv", show_col_types = F)
+uw_gtube <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/uw_gtubes.csv", show_col_types = F)
 uw_gtube <- uw_gtube %>%
   select(`CFF ID`) %>%
   distinct() %>%
@@ -241,7 +241,7 @@ df$`G-tube in past 12 months`[is.na(df$`G-tube in past 12 months`)] <- "No"
 # Pre-/post-modulator
 df$`Pre or Post` <- factor(sign(df$Time), levels = -1:1, labels = c("Pre", "Pre", "Post"))
 # Add insulin information
-chco_ins <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/chco_insulin.csv", show_col_types = F)
+chco_ins <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/chco_insulin.csv", show_col_types = F)
 chco_ins$`CFF ID` <- chco_a1c$`CFF ID`[match(chco_ins$MRN, chco_a1c$MRN)]
 chco_ins <- chco_ins %>%
   rename(
@@ -250,14 +250,14 @@ chco_ins <- chco_ins %>%
   ) %>%
   select(`CFF ID`, `Pre or Post`, TDD, `Long acting insulin dose`) %>%
   filter(!is.na(`CFF ID`))
-mon_ins <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/montana_insulin.csv", show_col_types = F)
+mon_ins <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/montana_insulin.csv", show_col_types = F)
 mon_ins <- mon_ins %>%
   rename(
     TDD = `Total Daily Dose of insulin (units)`,
     `Long acting insulin dose` = `Long acting or total basal daily dose (units)`
   ) %>%
   select(`CFF ID`, `Pre or Post`, TDD, `Long acting insulin dose`)
-uw_ins <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/uw_insulin.csv", show_col_types = F)
+uw_ins <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/uw_insulin.csv", show_col_types = F)
 uw_ins <- uw_ins %>%
   rename(
     TDD = `Total Daily Dose of insulin (units)`,
@@ -286,9 +286,9 @@ keep <- df %>%
   filter(n == 2) %>%
   pull(`CFF ID`)
 df <- df %>% filter(`CFF ID` %in% keep)
-# CGM data
+# CGM variables
 cgm_vars <- c(
-  "average_sensor", "min_sensor", "max_sensor",
+  "mean_sensor", "min_sensor", "max_sensor",
   "percent_time_over_180", "percent_time_70_180",
   "percent_time_over_120",
   "percent_time_over_140", "percent_time_over_200",
@@ -300,31 +300,44 @@ day_cgm <- c(
   "daytime_avg_sensor_glucose", "daytime_min_sensor_glucose",
   "daytime_max_sensor_glucose", "percent_time_over_180_day",
   "percent_time_70_180_day", "percent_time_over_200_day",
-  "percent_time_under_60_day", "percent_time_under_70_day", "daytime_sd"
+  "percent_time_under_60_day", "percent_time_under_70_day", 
+  "percent_time_over_140_day","daytime_sd"
 )
 night_cgm <- c(
   "nighttime_avg_sens_glucose", "nighttime_min_sens_glucose",
   "nighttime_max_sens_glucose", "percent_time_over_180_night",
   "percent_time_70_180_night", "percent_time_over_200_night",
-  "percent_time_under_60_night", "percent_time_under_70_night", "nighttime_sd"
+  "percent_time_under_60_night", "percent_time_under_70_night", 
+  "percent_time_over_140_night","nighttime_sd"
 )
 # Add CGM information
+cgmvariables("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Raw/Data From CHCO/clean CGM",
+             "/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned",outputname = "chco_cgm")
+cgmvariables("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Raw/Data From Montana/CGM_Montana/clean CGM",
+             "/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned",outputname = "montana_cgm")
+cgmvariables("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Raw/Data From Washington/cgm/clean CGM",
+             "/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned",outputname = "uw_cgm")
 # CHCO
-chco_cgm <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/chco_cgm.csv", show_col_types = F)
-chco_cgm$`Pre or Post` <- factor(sapply(str_split(chco_cgm$subject_id, "_"), "[", 2))
-levels(chco_cgm$`Pre or Post`) <- c("Post", "Pre", "Pre")
+chco_cgm <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/chco_cgm.csv", show_col_types = F)
+chco_cgm = chco_cgm[-c(5,8,10,16,21),]
+chco_cgm$`Pre or Post` <- factor(sapply(str_split(chco_cgm$subject_id, "_"), "[", 2),
+                                 levels = c("post","Post","pre" ,"Pre" ),
+                                 labels = c("Post","Post","Pre","Pre"))
 chco_cgm$`CFF ID` <- sapply(str_split(chco_cgm$subject_id, "_"), "[", 1)
 chco_cgm <- chco_cgm %>% select(`CFF ID`, `Pre or Post`, all_of(c(cgm_vars, day_cgm, night_cgm)))
 # Montana
-mon_cgm <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/montana_cgm.csv", show_col_types = F)
-mon_cgm$`Pre or Post` <- factor(sapply(str_split(mon_cgm$subject_id, "_"), "[", 2))
-levels(mon_cgm$`Pre or Post`) <- c("Post", "Post", "Pre", "Pre")
+mon_cgm <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/montana_cgm.csv", show_col_types = F)
+mon_cgm$`Pre or Post` <- factor(sapply(str_split(mon_cgm$subject_id, "_"), "[", 2),
+                                levels = c("post","Post","pre" ,"Pre" ),
+                                labels = c("Post","Post","Pre","Pre"))
 mon_cgm$`CFF ID` <- sapply(str_split(mon_cgm$subject_id, "_"), "[", 1)
 mon_cgm <- mon_cgm %>% select(`CFF ID`, `Pre or Post`, all_of(c(cgm_vars, day_cgm, night_cgm)))
 # UW
-uw_cgm <- read_csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/uw_cgm.csv", show_col_types = F)
-uw_cgm$`Pre or Post` <- factor(sapply(str_split(uw_cgm$subject_id, "_"), "[", 2))
-levels(uw_cgm$`Pre or Post`) <- c("Post", "Pre")
+uw_cgm <- read_csv("/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/uw_cgm.csv", show_col_types = F)
+uw_cgm = uw_cgm[-1,]
+uw_cgm$`Pre or Post` <- factor(sapply(str_split(uw_cgm$subject_id, "_"), "[", 2),
+                               levels = c("post","Post","pre" ,"Pre" ),
+                               labels = c("Post","Post","Pre","Pre"))
 uw_cgm$`CFF ID` <- sapply(str_split(uw_cgm$subject_id, "_"), "[", 1)
 uw_cgm <- uw_cgm %>% select(`CFF ID`, `Pre or Post`, all_of(c(cgm_vars, day_cgm, night_cgm)))
 # Put together
@@ -332,4 +345,4 @@ cgm <- bind_rows(chco_cgm, mon_cgm, uw_cgm)
 # Add to DF
 cgm$`CFF ID` <- as.numeric(cgm$`CFF ID`)
 df <- left_join(df, cgm)
-save(df, cgm_vars, day_cgm, night_cgm, file = "/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Vigers/CF/Colleen Wood/Prepost Triakfta/Data_Cleaned/analysis_dataset.rdata")
+save(df, cgm_vars, day_cgm, night_cgm, file = "/home/timvigers/OneDrive/Vigers/CF/Colleen Wood/Prepost Trikafta/Data_Cleaned/analysis_dataset.rdata")
