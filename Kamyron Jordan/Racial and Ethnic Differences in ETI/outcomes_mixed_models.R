6c69a4e6a684ead89b7ca39e66feeb956dacad5d
Tim Vigers <timothy.vigers@cuanschutz.edu>
2024-07-24 18:16:37 -0600

    mixed model predictions still not working

--- Kamyron Jordan/Racial and Ethnic Differences in ETI/outcomes_mixed_models.R
+++ Kamyron Jordan/Racial and Ethnic Differences in ETI/outcomes_mixed_models.R
@@ -1,22 +1,29 @@
 library(lme4)
+library(splines)
 library(ggeffects)
+# Paths
+home_dir <- switch(Sys.info()["sysname"],
+  "Darwin" = "/Users/timvigers/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/Vigers/CF",
+  "Windows" = "C:/Users/Tim/OneDrive - The University of Colorado Denver/Vigers/CF",
+  "Linux" = "/home/timvigers/OneDrive/Vigers/CF"
+)
+setwd(home_dir)
 # Load dataset
 load("./Kamyron Jordan/Racial and Ethnic Differences in ETI/Data_Cleaned/outcomes_dataset.RData")
-# Make a dataframe for the levels we want predictions at
-new_df <- expand.grid(
-  Days = c(min(encounter$Days), 0, max(encounter$Days)),
-  Race = unique(encounter$Race)
-)
 # Fit models
 results <- lapply(continuous_outcomes, function(o) {
   # Formula
   f <-
     as.formula(paste0(
       o,
-      "~ age_group + Insurance + bs(Days, knots = 0, degree = 1) * Race + (1 | eDWID)"
+      "~ bs(Days, knots = 0, degree = 1) * Race + (1 | eDWID)"
     ))
   m <- lmer(f, data = encounter)
   # Predicted values
-  pred <- predict_response(m, terms = new_df)
+  pred <- predict_response(m, terms = c("Days[-1095,0,730]", "Race"))
   return(list(model = m, predictions = pred))
 })
+# Save
+save(results,
+  file = "./Kamyron Jordan/Racial and Ethnic Differences in ETI/Data_Cleaned/outcomes_lmm_results.RData"
+)
