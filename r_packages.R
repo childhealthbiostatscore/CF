4d5742ba6bf8d4e93761d805692d668bad9b1d52
Tim Vigers <timothy.vigers@cuanschutz.edu>
2024-08-21 15:44:05 -0600

    remove r packages file

--- r_packages.R
+++
@@ -1,9 +0,0 @@
-# List of regular R packages
-cran <- c(
-  "tidyverse", "gtsummary", "styler", "mcp", "segmented", "kableExtra",
-  "redcapAPI", "caret", "lmerTest"
-)
-install.packages(cran)
-# Bioconductor
-bioc <- c("limma", "mixOmics")
-BiocManager::install(bioc)
